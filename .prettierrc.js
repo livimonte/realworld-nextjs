0d0251049c6571a3536db5b286303af571a49707
Livingstone Del Monte <livingstonemonte@fitec.org.br>
2022-04-17 18:48:36 -0300

    chore: update package.json and add setup files

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
