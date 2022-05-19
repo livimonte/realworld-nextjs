d74da854e6664dc8ff0ee81e7be0bcc21cf6cf12
Livingstone Del Monte <livingstonemonte@fitec.org.br>
2022-05-19 05:46:26 -0300

    refactoring: configure pages to be files with .page.tsx extension

--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -3,53 +3,52 @@ import * as Yup from 'yup'
 import { NextPage } from 'next'
 import { Link } from '../../components/_common/link/link'
 import { api } from '../../utils/api'
-import { conduitFormErrorsToString, log, getErrorMessage, handleError } from '../../utils/helpers'
+import { conduitFormErrorsToString, log, handleError } from '../../utils/helpers'
 import { ErrorResponse } from '../../utils/types'
+import { useRouter } from 'next/router'
 
-type RegisterData = {
-  username: string
+type LoginData = {
   email: string
   password: string
-  password_confirm: string
 }
 
-const RegisterSchema = Yup.object().shape({
-  username: Yup.string().required('Name is a required field'),
+const LoginSchema = Yup.object().shape({
   email: Yup.string().email('Email must be a valid email').required('Email is a required field'),
-  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is a required field'),
-  password_confirm: Yup.string()
-    .equals([Yup.ref('password')], 'Passwords must match')
-    .required('Confirm Password is a required field'),
+  password: Yup.string().required('Password is a required field'),
 })
 
-const Register: NextPage = () => {
-  const handleRegister = async (values: RegisterData) => {
-    const { username, email, password } = values
+const LoginPage: NextPage = () => {
+  const router = useRouter()
+
+  const handleLogin = async (values: LoginData) => {
+    const { email, password } = values
 
     try {
-      const user = await api.post('/users', {
-        user: { username, email, password },
+      const {
+        data: { user },
+      } = await api.post('/users/login', {
+        user: { email, password },
       })
 
-      log('user', user)
+      if (user && user.token && user.email) {
+        localStorage.setItem('@rw:user', JSON.stringify(user))
+        router.push('/')
+      }
     } catch (error) {
       const message = conduitFormErrorsToString(error as ErrorResponse)
-
-      // handleError(error)
+      log('mesage', message)
     }
   }
 
   const formik = useFormik({
     initialValues: {
-      username: '',
       email: '',
       password: '',
-      password_confirm: '',
     },
-    validationSchema: RegisterSchema,
+    validationSchema: LoginSchema,
     onSubmit: async (values) => {
-      await RegisterSchema.validate(values)
-      await handleRegister(values)
+      await LoginSchema.validate(values)
+      await handleLogin(values)
     },
   })
 
@@ -60,33 +59,16 @@ const Register: NextPage = () => {
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
-              <Link href="/auth/login">Have an account?</Link>
+              <Link href="/auth/register">Need an account?</Link>
             </p>
 
             <FormikProvider value={formik}>
               <ul className="error-messages">
-                {formik.touched.username && formik.errors.username && <li>{formik.errors.username}</li>}
                 {formik.touched.email && formik.errors.email && <li>{formik.errors.email}</li>}
                 {formik.touched.password && formik.errors.password && <li>{formik.errors.password}</li>}
-                {formik.touched.password_confirm && formik.errors.password_confirm && (
-                  <li>{formik.errors.password_confirm}</li>
-                )}
               </ul>
 
               <Form noValidate onSubmit={formik.handleSubmit}>
-                <fieldset className="form-group">
-                  <Field
-                    className="form-control form-control-lg"
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder="Full name"
-                    value={formik.values.username}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                </fieldset>
-
                 <fieldset className="form-group">
                   <Field
                     className="form-control form-control-lg"
@@ -113,19 +95,6 @@ const Register: NextPage = () => {
                   />
                 </fieldset>
 
-                <fieldset className="form-group">
-                  <Field
-                    className="form-control form-control-lg"
-                    id="password_confirm"
-                    name="password_confirm"
-                    type="password"
-                    placeholder="Confirm Password"
-                    value={formik.values.password_confirm}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                </fieldset>
-
                 <button type="submit" disabled={formik.isSubmitting} className="btn btn-lg btn-primary pull-xs-right">
                   Sign in
                 </button>
@@ -138,4 +107,4 @@ const Register: NextPage = () => {
   )
 }
 
-export default Register
+export default LoginPage
--- src/pages/auth/register.page.tsx
+++ src/pages/auth/register.page.tsx
@@ -3,52 +3,53 @@ import * as Yup from 'yup'
 import { NextPage } from 'next'
 import { Link } from '../../components/_common/link/link'
 import { api } from '../../utils/api'
-import { conduitFormErrorsToString, log, handleError } from '../../utils/helpers'
+import { conduitFormErrorsToString, log, getErrorMessage, handleError } from '../../utils/helpers'
 import { ErrorResponse } from '../../utils/types'
-import { useRouter } from 'next/router'
 
-type LoginData = {
+type RegisterData = {
+  username: string
   email: string
   password: string
+  password_confirm: string
 }
 
-const LoginSchema = Yup.object().shape({
+const RegisterSchema = Yup.object().shape({
+  username: Yup.string().required('Name is a required field'),
   email: Yup.string().email('Email must be a valid email').required('Email is a required field'),
-  password: Yup.string().required('Password is a required field'),
+  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is a required field'),
+  password_confirm: Yup.string()
+    .equals([Yup.ref('password')], 'Passwords must match')
+    .required('Confirm Password is a required field'),
 })
 
-const Login: NextPage = () => {
-  const router = useRouter()
-
-  const handleLogin = async (values: LoginData) => {
-    const { email, password } = values
+const RegisterPage: NextPage = () => {
+  const handleRegister = async (values: RegisterData) => {
+    const { username, email, password } = values
 
     try {
-      const {
-        data: { user },
-      } = await api.post('/users/login', {
-        user: { email, password },
+      const user = await api.post('/users', {
+        user: { username, email, password },
       })
 
-      if (user && user.token && user.email) {
-        localStorage.setItem('@rw:user', JSON.stringify(user))
-        router.push('/')
-      }
+      log('user', user)
     } catch (error) {
       const message = conduitFormErrorsToString(error as ErrorResponse)
-      log('mesage', message)
+
+      // handleError(error)
     }
   }
 
   const formik = useFormik({
     initialValues: {
+      username: '',
       email: '',
       password: '',
+      password_confirm: '',
     },
-    validationSchema: LoginSchema,
+    validationSchema: RegisterSchema,
     onSubmit: async (values) => {
-      await LoginSchema.validate(values)
-      await handleLogin(values)
+      await RegisterSchema.validate(values)
+      await handleRegister(values)
     },
   })
 
@@ -59,16 +60,33 @@ const Login: NextPage = () => {
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
-              <Link href="/auth/register">Need an account?</Link>
+              <Link href="/auth/login">Have an account?</Link>
             </p>
 
             <FormikProvider value={formik}>
               <ul className="error-messages">
+                {formik.touched.username && formik.errors.username && <li>{formik.errors.username}</li>}
                 {formik.touched.email && formik.errors.email && <li>{formik.errors.email}</li>}
                 {formik.touched.password && formik.errors.password && <li>{formik.errors.password}</li>}
+                {formik.touched.password_confirm && formik.errors.password_confirm && (
+                  <li>{formik.errors.password_confirm}</li>
+                )}
               </ul>
 
               <Form noValidate onSubmit={formik.handleSubmit}>
+                <fieldset className="form-group">
+                  <Field
+                    className="form-control form-control-lg"
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="Full name"
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </fieldset>
+
                 <fieldset className="form-group">
                   <Field
                     className="form-control form-control-lg"
@@ -95,6 +113,19 @@ const Login: NextPage = () => {
                   />
                 </fieldset>
 
+                <fieldset className="form-group">
+                  <Field
+                    className="form-control form-control-lg"
+                    id="password_confirm"
+                    name="password_confirm"
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={formik.values.password_confirm}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </fieldset>
+
                 <button type="submit" disabled={formik.isSubmitting} className="btn btn-lg btn-primary pull-xs-right">
                   Sign in
                 </button>
@@ -107,4 +138,4 @@ const Login: NextPage = () => {
   )
 }
 
-export default Login
+export default RegisterPage
